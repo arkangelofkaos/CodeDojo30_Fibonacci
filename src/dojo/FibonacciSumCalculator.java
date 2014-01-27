3f09671d95e4595ce94341885f53179860f9383d
arkangelofkaos <arkangelofkaos@gmail.com>
2014-01-27 22:56:24 +0000

    starting to implement full fibonacci sums calculator

--- src/dojo/FibonacciSumCalculator.java
+++ src/dojo/FibonacciSumCalculator.java
@@ -2,6 +2,7 @@ package dojo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 
@@ -11,19 +12,27 @@ import static java.util.Arrays.asList;
 public class FibonacciSumCalculator {
     private static final List<Integer> FIBONACCI_NUMBERS = asList(1, 2, 3, 5, 8, 13);
 
-    public List<Integer> fibonacciSumFor(int i) {
-        if (i <= 0) {
+    public List<List<Integer>> fibonacciSumsFor(int number) {
+        return asList(fibonacciSumFor(number));
+    }
+
+    public List<Integer> fibonacciSumFor(int number) {
+        if (number <= 0) {
             return asList();
         }
 
-        Integer maxFib = FIBONACCI_NUMBERS.stream()
-                .filter(x -> x <= i)
+        Integer maxFib = fibonacciStreamUpTo(number)
                 .max(Integer::compare)
                 .get();
 
         List<Integer> fibsSoFar = new ArrayList<>();
         fibsSoFar.add(maxFib);
-        fibsSoFar.addAll(fibonacciSumFor(i - maxFib));
+        fibsSoFar.addAll(fibonacciSumFor(number - maxFib));
         return fibsSoFar;
+    }
+
+    private Stream<Integer> fibonacciStreamUpTo(int i) {
+        return FIBONACCI_NUMBERS.stream()
+                .filter(x -> x <= i);
     }
 }
---
+++ test/dojo/FibonacciSumsTest.java
@@ -0,0 +1,47 @@
+package dojo;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+/**
+ * @author arkangelofkaos
+ */
+public class FibonacciSumsTest {
+
+    private FibonacciSumCalculator calculator;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        calculator = new FibonacciSumCalculator();
+    }
+
+    @Test(dataProvider = "fibTestData")
+    public void givenANumber_shouldReturnFibonacciSum(int number,
+                                                      List<List<Integer>> expectedSums) throws Exception {
+        List<List<Integer>> actualSums = calculator.fibonacciSumsFor(number);
+        for (List<Integer> expectedSum : expectedSums) {
+            assertThat(actualSums, contains(expectedSum));
+        }
+    }
+
+    @DataProvider(name = "fibTestData")
+    public Object[][] fibTestData() {
+        return new Object[][]{
+                {1, asList(asList(1))},
+                {2, asList(asList(2))},
+//                {3, asList(asList(3), asList(2,1))},
+//                {5, asList(asList(5), asList(3,2))},
+                {4, asList(asList(3, 1))},
+                {6, asList(asList(5, 1))},
+                {7, asList(asList(5, 2))},
+                {20, asList(asList(13, 5, 2))},
+        };
+    }
+}
